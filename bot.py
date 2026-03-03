f86084ad0caff5977e6de7faa04bafcc7ad7461a
elifnazuyanik <35enu07@gmail.com>
2026-03-03 18:08:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,20 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
+@bot.command()
+async def biktim(ctx):
+    await ctx.send(f'ben de. Hadi camdan atlayalım')
+
+@bot.command()
+async def dizi(ctx):
+    await ctx.send(f'hmm... The Amazing Dijital Circus, Murder Drones,Stranger Things ya da Umbrella Academy olabilir. Daha aklıma gelirse yazarım.')
+
+
+
 @bot.group()
 async def cool(ctx):
 
     if ctx.invoked_subcommand is None:
         await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
 bot.run(TOKEN)
+
